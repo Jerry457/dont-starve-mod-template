4f11283fbd45b6c10f8a81d33db0ae0ab3186d78
Jerry457 <2234834467@qq.com>
2025-04-05 02:55:36 +0800

    elixir_container update

--- postinit/prefabs/elixir_container.lua
+++ postinit/prefabs/elixir_container.lua
@@ -2,6 +2,15 @@ local AddPrefabPostInit = AddPrefabPostInit
 GLOBAL.setfenv(1, GLOBAL)
 
 local function OnOpen(inst, data)
+    local doer = data and data.doer or nil
+    local skilltreeupdater = (doer and doer.components.skilltreeupdater) or nil
+    if not skilltreeupdater then
+        return
+    end
+
+    if doer.prefab == "wendy" and doer.components.talker and not skilltreeupdater:IsActivated("wendy_potion_container") then
+        doer.components.talker:Say(GetString(doer, "USE_ELIXIR_CONTAINER_WITHNOT_SKILL"))
+    end
 end
 
 AddPrefabPostInit("elixir_container", function(inst)
