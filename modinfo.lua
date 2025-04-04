5cb8ea0751b1bacc91823769bc28cb8b1086f173
Jerry457 <2234834467@qq.com>
2025-04-05 00:43:59 +0800

    update

--- modinfo.lua
+++ modinfo.lua
@@ -1,6 +1,7 @@
 -- This information tells other players more about the mod
-name = "wendy_skilltree"  ---mod名字
+
 version = "0.0.2" -- mod版本 上传mod需要两次的版本不一样
+name = "wendy_skilltree(" .. version .. ")"  ---mod名字
 description = version .. ""  --mod描述
 author = "" --作者
 
