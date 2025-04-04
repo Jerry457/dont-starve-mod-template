3238af755691525342b6a124c17bd8ec155ec03a
Jerry457 <2234834467@qq.com>
2025-04-04 12:27:11 +0800

    fix bug

--- postinit/components/skilltreeupdater.lua
+++ postinit/components/skilltreeupdater.lua
@@ -1,7 +1,7 @@
 GLOBAL.setfenv(1, GLOBAL)
 
 local SkillTreeUpdater = require("components/skilltreeupdater")
-
+local SkillTreeBuilder = require("widgets/redux/skilltreebuilder")
 local skill_map = {
     wendy_smallghost_2 = "",
     wendy_smallghost_3 = "wendy_smallghost_1",
@@ -10,7 +10,15 @@ local skill_map = {
 }
 
 local _IsActivated = SkillTreeUpdater.IsActivated
-function SkillTreeUpdater:IsActivated(skill, ...)
-    skill = skill_map[skill] or skill
+local function IsActivated(self, skill, ...)
+    local skill = skill_map[skill]
     return _IsActivated(self, skill, ...)
+end
+SkillTreeUpdater.IsActivated = IsActivated
+
+local _RefreshTree = SkillTreeBuilder.RefreshTree
+function SkillTreeBuilder:RefreshTree(...)
+    SkillTreeUpdater.IsActivated = _IsActivated
+    _RefreshTree(self, ...)
+    SkillTreeUpdater.IsActivated = _IsActivated
 end
