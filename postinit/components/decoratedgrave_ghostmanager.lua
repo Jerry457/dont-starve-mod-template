424a002cc658c3806a885107086691477fd07e78
Jerry457 <2234834467@qq.com>
2025-04-07 14:27:10 +0800

    fix bug

--- postinit/components/decoratedgrave_ghostmanager.lua
+++ postinit/components/decoratedgrave_ghostmanager.lua
@@ -1,6 +1,11 @@
 local DecoratedGrave_GhostManager = require("components/decoratedgrave_ghostmanager")
 
-local OnUpdate = DecoratedGrave_GhostManager.OnUpdate
-function DecoratedGrave_GhostManager:OnUpdate(...)
-    return HookSkillTreeUpdaterIsActivated("wendy_gravestone_1", "wendy_smallghost_2", OnUpdate, self, ...)
-end
+local AddComponentPostInit = AddComponentPostInit
+GLOBAL.setfenv(1, GLOBAL)
+
+AddComponentPostInit("decoratedgrave_ghostmanager", function(cmp)
+    local _OnUpdate = cmp.OnUpdate
+    function cmp:OnUpdate(...)
+        return HookSkillTreeUpdaterIsActivated("wendy_gravestone_1", "wendy_smallghost_2", _OnUpdate, self, ...)
+    end
+end)
