d2d5f92759e524bbbad459212ebc23ccc507aeb6
Jerry457 <2234834467@qq.com>
2025-04-19 13:18:29 +0800

    fix bug

--- postinit/stategraphs/SGabigail.lua
+++ postinit/stategraphs/SGabigail.lua
@@ -104,13 +104,33 @@ AddStategraphPostInit("abigail", function(sg)
         inst.components.health:SetInvincible(false)
     end
 
-    -- local _gestalt_loop_homing_attack_onexit = sg.states["gestalt_loop_homing_attack"].onexit
-    -- sg.states["gestalt_loop_homing_attack"].onexit = function(inst, ...)
-    --     _gestalt_loop_homing_attack_onexit(inst, ...)
-    -- end
+    local _gestalt_loop_homing_attack_onenter = sg.states["gestalt_loop_attack"].onenter
+    sg.states["gestalt_loop_attack"].onenter = function(inst, ...)
+        local _GetDebuff = inst.GetDebuff
+        function inst:GetDebuff(name, ...)
+            local player_to_ghost_elixir_buff = _GetDebuff(self, "player_to_ghost_elixir_buff", ...)
+            if player_to_ghost_elixir_buff and player_to_ghost_elixir_buff.prefab == "ghostlyelixir_attack_buff" then
+                return player_to_ghost_elixir_buff
+            else
+                _GetDebuff(self, name, ...)
+            end
+        end
+        _gestalt_loop_homing_attack_onenter(inst, ...)
+        inst.GetDebuff = _GetDebuff
+    end
 
-    -- local _gestalt_loop_attack_onexit = sg.states["gestalt_loop_attack"].onexit
-    -- sg.states["gestalt_loop_attack"].onexit = function(inst, ...)
-    --     _gestalt_loop_attack_onexit(inst, ...)
-    -- end
+    local _gestalt_loop_attack_onenter = sg.states["gestalt_loop_homing_attack"].onenter
+    sg.states["gestalt_loop_homing_attack"].onenter = function(inst, ...)
+        local _GetDebuff = inst.GetDebuff
+        function inst:GetDebuff(name, ...)
+            local player_to_ghost_elixir_buff = _GetDebuff(self, "player_to_ghost_elixir_buff", ...)
+            if player_to_ghost_elixir_buff and player_to_ghost_elixir_buff.prefab == "ghostlyelixir_attack_buff" then
+                return player_to_ghost_elixir_buff
+            else
+                _GetDebuff(self, name, ...)
+            end
+        end
+        _gestalt_loop_attack_onenter(inst, ...)
+        inst.GetDebuff = _GetDebuff
+    end
 end)
