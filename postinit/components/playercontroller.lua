b2a70d2b90e46e2e2f3ff6f2f8534b907c3cddaa
Jerry457 <2234834467@qq.com>
2025-04-19 23:47:52 +0800

    fix bug

--- postinit/components/playercontroller.lua
+++ postinit/components/playercontroller.lua
@@ -6,10 +6,10 @@ local _RemoteMakeRecipeAtPoint = PlayerController.RemoteMakeRecipeAtPoint
 function PlayerController:RemoteMakeRecipeAtPoint(...)
     local _BufferedAction_ctor = BufferedAction._ctor
     BufferedAction._ctor = function(self, doer, target, action, invobject, pos, recipe, distance, ...)
+        _BufferedAction_ctor(self, doer, target, action, invobject, pos, recipe, distance, ...)
         if action == ACTIONS.BUILD then
             distance = recipe.build_distance
         end
-        _BufferedAction_ctor(self, doer, target, action, invobject, pos, recipe, distance, ...)
     end
 
     _RemoteMakeRecipeAtPoint(self, ...)
