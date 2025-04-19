0ee8b715295f949d9e574eccd5492a9ca2cda367
Jerry457 <2234834467@qq.com>
2025-04-19 22:14:51 +0800

    update

--- postinit/components/equippable.lua
+++ postinit/components/equippable.lua
@@ -8,7 +8,7 @@ function Equippable:GetWalkSpeedMult(...)
 
     local owner = self.inst.components.inventoryitem and self.inst.components.inventoryitem.owner
     if speed < 1 and self.isequipped and owner and owner:HasTag("ghostlyelixir_speed") then
-        speed = math.min(1, speed)
+        speed = math.max(1, speed)
     end
 
     return speed
--- postinit/components/inventoryitem_replica.lua
+++ postinit/components/inventoryitem_replica.lua
@@ -8,7 +8,7 @@ function InventoryItem:GetWalkSpeedMult(...)
 
     local owner = self.owner
     if speed_mult < 1 and ThePlayer and self:IsGrandOwner(ThePlayer) and ThePlayer:HasTag("ghostlyelixir_speed") then
-        speed_mult = math.min(1, speed_mult)
+        speed_mult = math.max(1, speed_mult)
     end
 
     return speed_mult
