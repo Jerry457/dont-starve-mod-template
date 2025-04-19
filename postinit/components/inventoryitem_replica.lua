99436a9ca0ffcb44c6767bf2acf76dbaccbea808
Jerry457 <2234834467@qq.com>
2025-04-19 20:34:57 +0800

    fix bug

--- postinit/components/inventoryitem_replica.lua
+++ postinit/components/inventoryitem_replica.lua
@@ -4,12 +4,12 @@ local InventoryItem = require("components/inventoryitem_replica")
 
 local _GetWalkSpeedMult = InventoryItem.GetWalkSpeedMult
 function InventoryItem:GetWalkSpeedMult(...)
-    local speed = _GetWalkSpeedMult(self, ...)
+    local speed_mult = _GetWalkSpeedMult(self, ...)
 
     local owner = self.owner
-    if speed < 1 and owner and owner:HasTag("ghostlyelixir_speed") then
-        speed = math.min(1, speed)
+    if speed_mult < 1 and ThePlayer and self:IsGrandOwner(ThePlayer) and ThePlayer:HasTag("ghostlyelixir_speed") then
+        speed_mult = math.min(1, speed_mult)
     end
 
-    return speed
+    return speed_mult
 end
