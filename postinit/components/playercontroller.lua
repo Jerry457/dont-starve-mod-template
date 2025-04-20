d0ce86092ba67ee22c80e21d6f65bcaef605d169
Jerry457 <2234834467@qq.com>
2025-04-20 12:12:57 +0800

    fix bug

--- postinit/components/playercontroller.lua
+++ postinit/components/playercontroller.lua
@@ -3,16 +3,16 @@ GLOBAL.setfenv(1, GLOBAL)
 local PlayerController = require("components/playercontroller")
 -- fuck klei
 local _RemoteMakeRecipeAtPoint = PlayerController.RemoteMakeRecipeAtPoint
-function PlayerController:RemoteMakeRecipeAtPoint(...)
+function PlayerController:RemoteMakeRecipeAtPoint(recipe, ...)
     local _BufferedAction_ctor = BufferedAction._ctor
-    BufferedAction._ctor = function(self, doer, target, action, invobject, pos, recipe, distance, ...)
-        _BufferedAction_ctor(self, doer, target, action, invobject, pos, recipe, distance, ...)
+    BufferedAction._ctor = function(self, doer, target, action, invobject, pos, recipe_name, distance, ...)
         if action == ACTIONS.BUILD then
             distance = recipe.build_distance
         end
+        _BufferedAction_ctor(self, doer, target, action, invobject, pos, recipe_name, distance, ...)
     end
 
-    _RemoteMakeRecipeAtPoint(self, ...)
+    _RemoteMakeRecipeAtPoint(self, recipe, ...)
 
     BufferedAction._ctor = _BufferedAction_ctor
 end
