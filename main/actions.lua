3ceed4aaf5fa170246b09f875681351758b09a75
Jerry457 <2234834467@qq.com>
2025-04-30 02:05:30 +0800

    fix bug

--- main/actions.lua
+++ main/actions.lua
@@ -160,7 +160,9 @@ end)
 AddComponentAction("SCENE", "knownlocations", function(inst, doer, actions, right)
     local skilltreeupdater = (doer and doer.components.skilltreeupdater) or nil
     if right and skilltreeupdater:IsActivated("wendy_smallghost_2") and inst and inst:HasTag("moon_tree_blossom_lantern") then
-        table.insert(actions, ACTIONS.HONOR_THE_MEMORY)
+        if TheWorld.Map:IsOceanAtPoint(inst.Transform:GetWorldPosition()) then
+            table.insert(actions, ACTIONS.HONOR_THE_MEMORY)
+        end
     end
 end)
 
