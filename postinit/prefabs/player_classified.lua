7b04ba0427d5428c712c293791ddd9dd5a1a1664
Jerry457 <2234834467@qq.com>
2025-05-01 00:00:36 +0800

    update

--- postinit/prefabs/player_classified.lua
+++ postinit/prefabs/player_classified.lua
@@ -26,28 +26,47 @@ local function OnMourningFlowerPercentDirty(inst)
     end
 end
 
+local function OnLockNightmarePhaseChange(inst, phase)
+    phase = phase and phase or ""
+    local atrium_gate = TheSim:FindFirstEntityWithTag("stargate")
+    if phase ~= "" and atrium_gate and atrium_gate.components.charliecutscene and atrium_gate.components.charliecutscene:IsGateRepaired() then
+        phase = phase .. "_repaired"
+    end
+    inst.locknightmarephase:set_local(phase)
+    inst.locknightmarephase:set(phase)
+end
+
+local function OnLockNightmarePhaseDirty(inst)
+    inst._parent:PushEvent("locknightmarephasechange", inst.locknightmarephase:value())
+end
+
 local function RegisterNetListeners(inst)
     if TheWorld.ismastersim then
         inst._parent = inst.entity:GetParent()
         inst:ListenForEvent("sisturnperishchange", OnSisturnperishChange, inst._parent)
         inst:ListenForEvent("mourningflowerpercentchange", OnMourningFlowerPercentChange, inst._parent)
+        inst:ListenForEvent("ms_locknightmarephase", function(src, phase)
+            OnLockNightmarePhaseChange(inst, phase)
+        end, TheWorld)
     else
         inst:ListenForEvent("sisturnperishdirty", OnSisturnPerishDirty)
         inst:ListenForEvent("mourningflowerpercentdirty", OnMourningFlowerPercentDirty)
     end
 
     if not TheNet:IsDedicated() then
-
+        inst:ListenForEvent("locknightmarephasedirty", OnLockNightmarePhaseDirty)
     end
 end
 
 AddPrefabPostInit("player_classified", function(inst)
     inst.sisturnstate = net_string(inst.GUID, "sisturn.state", "sisturnperishdirty")
     inst.sisturnperish = net_float(inst.GUID, "sisturn.perish", "sisturnperishdirty")
+    inst.locknightmarephase = net_string(inst.GUID, "lock.nightmarephase", "locknightmarephasedirty")
 
     inst.mourningflowerpercent = net_float(inst.GUID, "mourningflower.perish", "mourningflowerpercentdirty")
     inst.mourningflowerlight = net_bool(inst.GUID, "mourningflower.light", "mourningflowerpercentdirty")
 
+    inst.locknightmarephase:set("")
     inst.sisturnstate:set("NORMAL")
     inst.sisturnperish:set(0)
     inst.mourningflowerpercent:set(0)
--- scripts/widgets/spiritual_perception.lua
+++ scripts/widgets/spiritual_perception.lua
@@ -3,6 +3,7 @@
 local Widget = require("widgets/widget")
 local UIAnim = require("widgets/uianim")
 
+
 local function OnSkillChange(self)
     if self.owner.components.skilltreeupdater and self.owner.components.skilltreeupdater:IsActivated("wendy_avenging_ghost") then
         self:Show()
@@ -11,23 +12,63 @@ local function OnSkillChange(self)
     end
 end
 
-local function OnMoonPhaseChanged(self)
+local function OnMoonPhaseChange(self, data)
+    local style = data and data.style
     local symbol = "moon_" .. TheWorld.state.moonphase
-    print("moon phase changed to " .. symbol)
     local animation
-    if TheWorld.state.moonphase == "full" then
+    local fx_animation
+    if data then
+        self.alter = style == "alter_active" or style == "glassed_alter_active"
+    end
+    if self.alter then
+        animation = "alter"
+        fx_animation = "waxing"
+    elseif TheWorld.state.moonphase == "full" then
         animation = "moon_full"
+        fx_animation = "waxing"
     elseif TheWorld.state.moonphase == "new" then
         animation = "moon_new"
+        fx_animation = "waning"
     elseif TheWorld.state.iswaxingmoon then
         symbol = symbol .. "_waxing"
         animation = "moon_waxing"
+        fx_animation = "waxing"
     else
         symbol = symbol .. "_waning"
         animation = "moon_waning"
+        fx_animation = "waning"
     end
+
+    self:PlayerChangeFx(fx_animation)
     self.spiritual_perception:GetAnimState():OverrideSymbol("moon_phase", "spiritual_perception_moon_phase", symbol)
     self.spiritual_perception:GetAnimState():PlayAnimation(animation, true)
+end
+
+local function force_nightmare_wild_anim(inst)
+    if math.random(0, 1) <= 0.5 then
+        inst.AnimState:PlayAnimation("force_nightmare_wild_idle")
+    else
+        inst.AnimState:PlayAnimation("force_nightmare_wild_taunt")
+    end
+end
+
+local function OnNightMarePhaseChange(self)
+    if self.lisented then
+        self.lisented = false
+        self.spiritual_perception.inst:RemoveEventCallback("animover", force_nightmare_wild_anim)
+    end
+
+    self:PlayerChangeFx("nightmare")
+    if self.owner.player_classified.locknightmarephase:value() == "wild" then
+        self.spiritual_perception:GetAnimState():PlayAnimation("force_nightmare_wild_broken", true)
+    elseif self.owner.player_classified.locknightmarephase:value() == "wild_repaired" then
+        self.spiritual_perception:GetAnimState():PlayAnimation("force_nightmare_wild_idle")
+        self.lisented = true
+        self.spiritual_perception.inst:ListenForEvent("animover", force_nightmare_wild_anim)
+    else
+        local animation = "nightmare_" .. TheWorld.state.nightmarephase:gsub("nightmare", "nightmare_")
+        self.spiritual_perception:GetAnimState():PlayAnimation(animation, true)
+    end
 end
 
 local SpiritualPerception =  Class(Widget, function(self, owner)
@@ -38,21 +79,40 @@ local SpiritualPerception =  Class(Widget, function(self, owner)
     self.spiritual_perception:GetAnimState():SetBank("spiritual_perception")
     self.spiritual_perception:GetAnimState():SetBuild("spiritual_perception")
 
+    self.change_fx = self:AddChild(UIAnim())
+    self.change_fx:GetAnimState():SetBank("spiritual_perception")
+    self.change_fx:GetAnimState():SetBuild("spiritual_perception")
+
+    self._OnNightMarePhaseChange = function()
+        OnNightMarePhaseChange(self)
+    end
+    self._OnMoonPhaseChange = function(src, data)
+        OnMoonPhaseChange(self, data)
+    end
+
+    if TheWorld:HasTag("cave") then
+        self.inst:ListenForEvent("locknightmarephasechange", self._OnNightMarePhaseChange, owner)
+        self.inst:WatchWorldState("nightmarephase", self._OnNightMarePhaseChange)
+    else
+        self.inst:WatchWorldState("moonphase", self._OnMoonPhaseChange)
+        self.inst:ListenForEvent("moonphasestylechanged", self._OnMoonPhaseChange, TheWorld)
+    end
+
     self._OnSkillChange = function()
         OnSkillChange(self)
-    end
-    self._OnMoonPhaseChanged = function()
-        OnMoonPhaseChanged(self)
+        if TheWorld:HasTag("cave") then
+            OnNightMarePhaseChange(self)
+        else
+            OnMoonPhaseChange(self)
+        end
     end
     self.inst:ListenForEvent("onactivateskill_client", self._OnSkillChange, owner)
     self.inst:ListenForEvent("ondeactivateskill_client", self._OnSkillChange, owner)
-    self.inst:WatchWorldState("moonphase", self._OnMoonPhaseChanged)
-
-    self.inst:DoTaskInTime(0, function ()
-        self._OnSkillChange()
-        self._OnMoonPhaseChanged()
-    end)
+    self.inst:DoTaskInTime(0, self._OnSkillChange)
 end)
 
+function SpiritualPerception:PlayerChangeFx(animation)
+    self.change_fx:GetAnimState():PlayAnimation(animation)
+end
 
 return SpiritualPerception
