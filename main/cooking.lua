69ac853c95e25539aa7ae0e8c99f7494ecbcd37b
Jerry457 <2234834467@qq.com>
2025-05-02 16:37:32 +0800

    fix bug

--- main/cooking.lua
+++ main/cooking.lua
@@ -1,7 +1,11 @@
 local AddIngredientValues = AddIngredientValues
 GLOBAL.setfenv(1, GLOBAL)
 
-local cooking = require("cooking")
-local sewing = require("yotb_sewing")
+local preparedfoods = require("preparedfoods")
+preparedfoods.butterflymuffin.test = function(cooker, names, tags)
+    return (names.butterflywings or names.moonbutterflywings or names.fullmoonbutterflywings)
+        and not tags.meat
+        and tags.veggie and tags.veggie >= 0.5
+end
 
 AddIngredientValues({"fullmoonbutterflywings"}, {decoration=2})
